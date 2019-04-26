04f54c814c3dfeb2cc85526674f0d3fed95c94d3
mupezzuol <murillopezzuol@hotmail.com>
2019-04-26 18:26:50 -0300

    subindo projeto

---
+++ src/main/java/com/springboot/Spring5Boot2DataMongodbThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Boot2DataMongodbThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Boot2DataMongodbThymeleafApplication.class, args);
+	}
+
+}
